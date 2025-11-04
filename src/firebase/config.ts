6dc64a5bca2088877568f41f96084f74cd0cacc3
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-11-04 01:40:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9992002164-70162",
+  "appId": "1:728978668265:web:ad960e431d2e3c73b20e78",
+  "apiKey": "AIzaSyCsxeuekJec3_mkQDLj5tctWjkzXq-cnXI",
+  "authDomain": "studio-9992002164-70162.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "728978668265"
+};
