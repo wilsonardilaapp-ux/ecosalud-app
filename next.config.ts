b949c1c38eab8bda5cbe0c44338eb3d223cced3e
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-11-05 02:17:21 +0000

    Primero mira la imagen, se quedo cargado y no se inicia la  pagina. reví

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https://',
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
