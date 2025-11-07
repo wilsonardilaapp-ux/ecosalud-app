9053a52f5bde3350ae353126a7b50cfaeade3492
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-11-07 18:28:17 +0000

    MIRA LA IAMGNE SALE OTRO ERROR:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-9992002164-97900",
-  "appId": "1:728978668265:web:ad960e431d2e3c73b20e78",
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  "authDomain": "studio-9992002164-97900.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "728978668265"
+  "projectId": "gen-lang-client-0895449335",
+  "appId": "1:357613718531:web:d8759602738d01b17b7e6a",
+  "apiKey": "AIzaSyAoPplpR82a6y79i72PA9-2tC39T1aPDKg",
+  "authDomain": "gen-lang-client-0895449335.firebaseapp.com",
+  "measurementId": "G-5Y29R40P07",
+  "messagingSenderId": "357613718531"
 };
