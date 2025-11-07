929f52240fbf6ad4fbb82b566bea66f911424559
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-11-07 02:11:42 +0000

    revisa este guia sugerencida por que persiste el problena y no es de reg

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9992002164-70162",
+  "projectId": "studio-9992002164-97900",
   "appId": "1:728978668265:web:ad960e431d2e3c73b20e78",
   "apiKey": "AIzaSyCsxeuekJec3_mkQDLj5tctWjkzXq-cnXI",
-  "authDomain": "studio-9992002164-70162.firebaseapp.com",
+  "authDomain": "studio-9992002164-97900.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "728978668265"
 };
