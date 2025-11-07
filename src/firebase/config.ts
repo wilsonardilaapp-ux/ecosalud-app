f7232931a3488d6e7e10cb6c0b19c5ef1a48ce10
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-11-07 18:35:28 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  "projectId": "gen-lang-client-0895449335",
-  "appId": "1:357613718531:web:d8759602738d01b17b7e6a",
-  "apiKey": "AIzaSyAoPplpR82a6y79i72PA9-2tC39T1aPDKg",
-  "authDomain": "gen-lang-client-0895449335.firebaseapp.com",
-  "measurementId": "G-5Y29R40P07",
-  "messagingSenderId": "357613718531"
+  "projectId": "studio-9992002164-97900",
+  "appId": "1:291907867309:web:6218e0ee9a6386383dac24",
+  "apiKey": "AIzaSyBFArIq0M9lTmml2wQYlNArMk87ZddSYdU",
+  "authDomain": "studio-9992002164-97900.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "291907867309"
 };
