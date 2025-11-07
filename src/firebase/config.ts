fc5de40ba9ce53c991a1252d8c6a7e7f1db9f795
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-11-07 18:21:22 +0000

    mira la imagen sale error al inciar sesion. revisa afondo el problema o

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-9992002164-97900",
   "appId": "1:728978668265:web:ad960e431d2e3c73b20e78",
-  "apiKey": "AIzaSyDJHkt7crkyq5Uhtge-A0vusmc9395rFXY",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-9992002164-97900.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "728978668265"
