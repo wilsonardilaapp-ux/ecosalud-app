d8a87dcf3f2e38727e4825c85d1bcc0ef28bcf6c
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-11-10 14:35:42 +0000

    Primero mira el error, salio al dar clic  agregar el testimonios, en la

--- next.config.ts
+++ next.config.ts
@@ -47,6 +47,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
