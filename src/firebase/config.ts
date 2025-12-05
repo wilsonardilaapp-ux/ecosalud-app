4d9f6bfb8a134220dee09e5533ca72cd1607f6b4
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-12-05 23:50:37 +0000

    dime es lo que necesitoas:// Import the functions you need from the SDKs

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-9992002164-97900",
-  "appId": "1:291907867309:web:6218e0ee9a6386383dac24",
   "apiKey": "AIzaSyBFArIq0M9lTmml2wQYlNArMk87ZddSYdU",
   "authDomain": "studio-9992002164-97900.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "291907867309"
+  "projectId": "studio-9992002164-97900",
+  "storageBucket": "studio-9992002164-97900.appspot.com",
+  "messagingSenderId": "291907867309",
+  "appId": "1:291907867309:web:6218e0ee9a6386383dac24"
 };
