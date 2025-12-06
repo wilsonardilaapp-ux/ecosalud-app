180d78197624fc415181349eb17ba73d8e80c896
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-12-06 20:06:12 +0000

    dime ya aplicaste la corrección anterior si o no.

--- src/components/catalogo/product-form.tsx
+++ src/components/catalogo/product-form.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { useState, useEffect } from 'react';
@@ -254,5 +253,3 @@ export default function ProductForm({ product, onSave, onCancel }: ProductFormPr
         </form>
     );
 }
-
-    
