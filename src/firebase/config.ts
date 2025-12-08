1a458b55098e05a85d4e3b8b4c7feba00388c4c4
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-12-08 23:17:45 +0000

    aqui este enti estos datos laa aplicaicon si te sirve de lago para resol

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 
 export const firebaseConfig = {
-  "apiKey": "AIzaSyDNmfL6GstTjSjI3cE-8Fqg3g71pU9z5Bw",
-  "authDomain": "studio-9992002164.firebaseapp.com",
-  "projectId": "studio-9992002164",
-  "storageBucket": "studio-9992002164.appspot.com",
-  "messagingSenderId": "473523773175",
-  "appId": "1:473523773175:web:2d2e1b8b9da039d52462e7"
+  "apiKey": "AIzaSyBFArIq0M9lTmml2wQYlNArMk87ZddSYdU",
+  "authDomain": "studio-9992002164-97900.firebaseapp.com",
+  "projectId": "studio-9992002164-97900",
+  "storageBucket": "studio-9992002164-97900.appspot.com",
+  "messagingSenderId": "291907867309",
+  "appId": "1:291907867309:web:6218e0ee9a6386383dac24"
 };
-
