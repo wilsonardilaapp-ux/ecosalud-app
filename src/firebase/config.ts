9d0cf582fc6e5477c36b3ae9904142a8b4f711df
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-12-08 23:40:58 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBFArIq0M9lTmml2wQYlNArMk87ZddSYdU",
+  authDomain: "studio-9992002164-97900.firebaseapp.com",
+  projectId: "studio-9992002164-97900",
+  storageBucket: "studio-9992002164-97900.appspot.com",
+  messagingSenderId: "291907867309",
+  appId: "1:291907867309:web:6218e0ee9a6386383dac24"
 };
