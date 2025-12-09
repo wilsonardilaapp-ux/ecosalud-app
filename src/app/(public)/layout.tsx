b6ef7abaf6a90e6f0b4fd47ac296d85e743bb970
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-12-09 15:41:26 +0000

    Primero mira la pagina de inicio me esta mostrado 2 barra de navegación

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,11 +1,9 @@
 import type { ReactNode } from "react";
-import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 
 export default function PublicLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
