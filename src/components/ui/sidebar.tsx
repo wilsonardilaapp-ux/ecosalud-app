b55679038441ede2ba8f8578cbe95c355a17276c
Wilson Ardila <wilsonardilaapp@gmail.com>
2025-12-11 23:46:15 +0000

    primero mira la imagen, a pesar de la corrección anterior, no refleja lo

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -326,7 +326,7 @@ const SidebarInset = React.forwardRef<
       ref={ref}
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background",
-        "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
+        "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:my-2 md:peer-data-[variant=inset]:mr-2 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
       {...props}
